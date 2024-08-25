16b1f62b521e8aa6f878bd96103cb26d347d33bb
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-08-25 22:54:38 +0300

    middleware

--- src/twit/twit.controller.js
+++ src/twit/twit.controller.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { TwitService } from "./twit.service.js";
+import {authMiddleware} from "../auth.middleware.js";
 
 export const twitRouter = Router();
 
 const twitService = new TwitService();
 
-twitRouter.post("/", (req, res) => {
+twitRouter.post("/", authMiddleware, (req, res) => {
   const twit = twitService.createTwit(req.body);
   return res.status(201).json(twit);
 });
