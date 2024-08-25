a99bd9b2a0dd271f6a210164f005344b3fdbbde4
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-08-25 21:39:24 +0300

    implemented server js

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+const app = express();
+
+async function main() {
+  app.use(express.json());
+
+  app.use("/api/twit", (req, res) => {
+    res
+      .json({
+        message: "success",
+      })
+      .status(200);
+  });
+
+  app.listen(3021, () => {
+    console.log("Server is running");
+  });
+}
+
+main();
