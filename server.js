0464d8de70d205f3a57452fa93fbbfd948c8cbe2
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-08-25 22:44:25 +0300

    render view

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 import express from "express";
 import { twitRouter } from "./src/twit/twit.controller.js";
 import dotenv from "dotenv";
+import * as path from "path";
 
 dotenv.config();
 
 const app = express();
 
+app.set("views", './src/views');
+app.set("view engine", "ejs");
+
 async function main() {
   app.use(express.json());
 
   app.use("/api/twits", twitRouter);
+
+  app.get("/profile", (req, res) => {
+    res.render("profile", {
+      user: {
+        name: "John",
+        age: 23,
+      },
+    });
+  });
 
   app.all("*", (req, res) => {
     res.status(404).json({ message: "Not Found" });
