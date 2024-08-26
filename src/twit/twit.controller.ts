d510d2d2b2e68b0318a6bfe28eb4f19fbc84a68b
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-08-26 21:49:02 +0300

    added validation

--- src/twit/twit.controller.ts
+++ src/twit/twit.controller.ts
@@ -1,12 +1,17 @@
-import {Request, Response, Router} from "express";
+import { Request, Response, Router } from "express";
 import { TwitService } from "./twit.service";
-import {authMiddleware} from "@/auth.middleware";
+import { authMiddleware } from "@/auth.middleware";
+import { createTwitDto } from "@/twit/twit.dto";
 
 export const twitRouter = Router();
 
 const twitService = new TwitService();
 
 twitRouter.post("/", authMiddleware, (req: Request, res: Response) => {
+  const validation = createTwitDto.safeParse(req.body);
+  if (!validation.success) {
+    return res.status(400).json({ errors: validation.error.errors });
+  }
   const twit = twitService.createTwit(req.body);
   return res.status(201).json(twit);
 });
