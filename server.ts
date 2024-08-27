c4fbda525655b4db7c1c7eb79ac79bb7c2e70596
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-08-27 22:35:21 +0300

    added compression

--- server.ts
+++ server.ts
@@ -4,6 +4,7 @@ import { twitRouter } from "@/twit/twit.controller";
 import { PrismaClient } from "@prisma/client";
 import { logger } from "./src/utils/log";
 import helmet from "helmet";
+import compression from "compression";
 
 dotenv.config();
 
@@ -15,6 +16,7 @@ app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 app.use(helmet());
+app.use(compression());
 app.use(express.json());
 app.use("/api/twits", twitRouter);
 app.get("/profile", (req: Request, res: Response) => {
