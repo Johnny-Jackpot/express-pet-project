060e7b5d097a3fb853c1522a4e7395787472eead
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-08-27 22:04:03 +0300

    added logger

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import dotenv from "dotenv";
 import { twitRouter } from "@/twit/twit.controller";
 import { PrismaClient } from "@prisma/client";
@@ -13,46 +13,37 @@ const prisma = new PrismaClient();
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
-async function main() {
-  app.use(express.json());
-
-  app.use("/api/twits", twitRouter);
-
-  app.get("/profile", (req: Request, res: Response) => {
-    res.render("profile", {
-      user: {
-        name: "John",
-        age: 23,
-      },
-    });
+app.use(express.json());
+app.use("/api/twits", twitRouter);
+app.get("/profile", (req: Request, res: Response) => {
+  res.render("profile", {
+    user: {
+      name: "John",
+      age: 23,
+    },
   });
+});
+app.get("/error", (req: Request, res: Response, next) => {
+  throw new Error("Example error");
+});
 
-  app.get("/error", (req: Request, res: Response) => {
-    throw new Error("Test error");
-  });
+app.all("*", (req: Request, res: Response) => {
+  res.status(404).json({ message: "Not Found" });
+});
 
-  app.all("*", (req: Request, res: Response) => {
-    res.status(404).json({ message: "Not Found" });
-  });
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  logger.error(err.stack);
+  res.status(500).json({ message: "something wend wrong" });
+});
 
-  app.use((err: Error, req: Request, res: Response) => {
-    logger.error(err.stack);
-    res.status(500).json({ message: "something wend wrong" });
-  });
+const port = process.env.PORT ?? 4200;
 
-  const port = process.env.PORT ?? 4200;
+app.listen(port, () => {
+  logger.info(`Server is running at: http://localhost:${port}`);
+});
 
-  app.listen(port, () => {
-    logger.info(`Server is running at: http://localhost:${port}`);
-  });
-}
-
-main()
-  .then(async () => {
-    await prisma.$connect();
-  })
-  .catch(async (e) => {
-    logger.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+prisma.$connect().catch(async (e) => {
+  logger.error(e);
+  await prisma.$disconnect();
+  process.exit(1);
+});
